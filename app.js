30f9d8acb814c6926533bb595537e9f4d5666ce2
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-23 18:25:36 +0200

    add some debugging options

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+
 const port = 3000;
 const host = 'localhost';
+
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello from my library app');
 });
 
 app.listen(port, ()=>{
-  console.log(`listening on ${host}:${port}`);
+  debug(`listening on ${chalk.magenta(port)}`);
 })
