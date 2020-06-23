e89e15c5ae451ca51ad0dbb4062fe87d433a4120
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-23 20:29:30 +0200

    add eslint, nodemon and refactor the code to avoid eslint erros

--- app.js
+++ app.js
@@ -6,19 +6,18 @@ const path = require('path');
 
 const app = express();
 
-const port = 3000;
-const host = 'localhost';
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/css',express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views','index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(port, () => {
   debug(`listening on ${chalk.magenta(port)}`);
-})
+});
