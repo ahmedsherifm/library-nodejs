22657357e12b1e256e5470391f6e4c346a5014a4
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-23 18:36:28 +0200

    serving index

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -11,9 +12,9 @@ const host = 'localhost';
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from my library app');
+  res.sendFile(path.join(__dirname, 'views','index.html'));
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   debug(`listening on ${chalk.magenta(port)}`);
 })
