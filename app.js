1277ac25dc786b843245c5d8cdd66e8c27f4bd49
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-24 16:31:55 +0200

    add basic book router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,6 +18,16 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('Hello Books');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('Hello single book');
+  });
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render(
     'index',
