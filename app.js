92a6ff556c7b20917b61a58a974edbdaa00521a4
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-24 15:09:52 +0200

    add pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
@@ -13,9 +14,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
