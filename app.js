4c913ad37b11b4632a7980629f6603856f63b05d
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-24 16:48:52 +0200

    add books.ejs

--- app.js
+++ app.js
@@ -18,9 +18,52 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Histroical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Histroical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'Harry Potter and the Prisoner of Azkaban',
+    genre: 'Fiction',
+    author: 'J. K. Rowling',
+    read: false
+  },
+  {
+    title: 'A Song of Ice and Fire',
+    genre: 'Fiction',
+    author: 'George R. R. Martin',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  }
+];
+
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('Hello Books');
+    res.render(
+      'books',
+      {
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }
+        ],
+        title: 'Library',
+        books
+      }
+    );
   });
 
 bookRouter.route('/single')
