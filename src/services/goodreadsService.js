eae3f318e377be6d20e8037c0cf419b820eff999
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2020-06-25 16:54:35 +0200

    get books details from goodreads api

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
 function goodreadsService() {
   function getBookById(bookId) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'book description' });
+      axios.get(`https://www.goodreads.com/book/show/${bookId}.xml?key=bjp7wLXmot0N58yLzssisA`)
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
